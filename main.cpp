4f64ecea66ef3f413f72fd4c73f1fdc62853089f
Szasz Attila <attilaszia@Szaszs-MacBook-Pro.local>
2020-07-10 19:30:01 +0200

    Remove

--- main.cpp
+++ main.cpp
@@ -58,10 +58,6 @@ Uint32 my_callbackfunc2(Uint32 interval, void* pixels)
 
     Uint32* actual_pixels = (Uint32*)pixels;
 
-    /* In this example, our callback pushes an SDL_USEREVENT event
-    into the queue, and causes our callback to be called again at the
-    same interval: */
-
     userevent.type = SDL_USEREVENT;
     userevent.code = 1;
     userevent.data1 = NULL;
